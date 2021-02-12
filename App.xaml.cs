99bacf39441482a5745d36f424ffde0a1d218831
Daniel Charles <daniel.charles@schneider-electric.com>
2021-02-12 13:10:24 -0800

    Add simple app.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ItsaDrag
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ItsaDrag
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TreeViewItem sourceItem;
+        private TreeViewItem targetItem;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TreeViewItem_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+            if (!(sender is TreeViewItem rootTreeViewItem))
+            {
+                return;
+            }
+            var treeView = FindParent<TreeView>(rootTreeViewItem);
+            sourceItem = treeView.SelectedItem as TreeViewItem;
+            var dropEffect = DragDrop.DoDragDrop(treeView, sourceItem, DragDropEffects.Move);
+            if (dropEffect != DragDropEffects.Move || targetItem == null || sourceItem == targetItem)
+            {
+                return;
+            }
+            DropInto(sourceItem, targetItem);
+        }
+
+        private void DropInto(TreeViewItem draggedItem, TreeViewItem droppedItem)
+        {
+            if (FindParent<TreeViewItem>(draggedItem) is TreeViewItem parent)
+            {
+                parent.Items.Remove(draggedItem);
+            }
+            droppedItem.Items.Add(draggedItem);
+        }
+
+        private void TreeViewItem_DragOver(object sender, DragEventArgs e)
+        {
+            if (FindParent<TreeViewItem>(e.OriginalSource as DependencyObject) is TreeViewItem overItem)
+            {
+                e.Handled = true;
+                e.Effects = IsValidDropTarget(sourceItem, overItem) ? DragDropEffects.Move : DragDropEffects.None;
+            }
+        }
+
+        private void TreeViewItem_Drop(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.None;
+            e.Handled = true;
+            if (sourceItem is null)
+            {
+                return;
+            }
+            if (FindParent<TreeViewItem>(sender as DependencyObject) is TreeViewItem dropTarget)
+            {
+                targetItem = dropTarget;
+                e.Effects = DragDropEffects.Move;
+            }
+        }
+
+        private static T FindParent<T>(DependencyObject child) where T: DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(child);
+            if (parent is T desiredParent)
+            {
+                return desiredParent;
+            }
+            else if (parent is null)
+            {
+                return null;
+            }
+            return FindParent<T>(parent);
+        }
+
+        private static bool IsValidDropTarget(TreeViewItem draggedItem, TreeViewItem potentialDropItem)
+        {
+            return draggedItem != potentialDropItem;
+        }
+    }
+}
